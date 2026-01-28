ad087a9e92278aa68a74237002663d03d6a5c064
aisha-siddique-1410 <aisha.siddique@anudip.org>
2026-01-28 17:56:08 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,193 @@
+// console.log("Hello world");
+// console.log("Hello world");
+// console.log("Hello world");
+// console.log("Hello world");
+// console.log("Hello world");
+
+// firstname = "Aisha";
+// age = 24;
+// price = 99.99;
+// x= null;
+// y= undefined;
+// console.log(firstname);
+
+
+// var age = 24;
+
+// var age = 39;
+
+// var age = 24;
+
+// var age = 45;
+
+// console.log(age);
+
+// var name = "Aisha";
+// // 100s line of code
+// var name = "Swasti";
+
+// var name = 99.99;
+
+// var name = null;
+
+// let age = 24;
+
+// let age = 35
+
+// update
+
+// age = 35;
+// const pi = 3.14
+
+
+// const age = 24;
+// age = 35;
+
+// console.log(age);
+
+// const a;
+// a= 10;
+// console.log(a);
+
+// scope
+// {
+//     var a= 5; // global scope
+//     console.log(a);
+// }
+// console.log(a);
+
+// {
+//     let a= 5; // block scope, const also
+//     console.log(a);
+// }
+// console.log(a);
+/* multiline comments */
+
+// let a= 10;
+// let b= "10";
+// console.log(a==b); // equal to
+
+// strict comparison: check value as  well as the type
+// console.log(a===b);
+
+
+// for loop
+// for (let i=1; i<=10; i++){
+//     console.log("hello world")
+// }
+
+// while loop
+
+// let i=1;
+// while (i<=5){
+//     console.log("hello world");
+//     i++; // 2 3 4 5 6
+// }
+
+// do while loop
+// let i=10;
+// do {
+//      console.log("hello world")
+//      i++;
+// }while(i<=5);
+
+// For... of : values
+// let str = "HelloWorld";
+
+// for(let i of str){
+//     console.log(i);
+// }
+
+// for.. in : Keys /indexes
+
+// let student = {
+//     name : "Rahul Kumar",
+//     age : 20,
+//     cgpa : 7.5,
+//     isPass : true
+// };
+
+// for(let i in student){
+//     // console.log("key=",i, "value =", student[i]);
+//     // if(i== "age")
+//     // console.log(student["age"]);
+//     console.log(student.name);
+// }
+
+// console.log("hello world");
+// // alert("hello world");
+// let num = prompt("Enter a number");
+// console.log(num);
+
+// let gameNum = 25;
+// let userNum = prompt("Guess the number: ");
+
+// while(userNum != gameNum){
+//     userNum= prompt("You entered wrong number, guess again");
+
+// }
+
+// console.log("Congratulations! you are the winner")
+
+
+// template literals
+// let str= `this is a template literal`;
+// console.log(str);
+
+// let obj = {
+//     item : "pen",
+//     price : 10
+// };
+
+// console.log("The cost of", obj.item, " is",obj.price );
+
+// // template literals
+
+// console.log(`The cost of ${obj.item} is ${obj.price}`);
+
+// arrays
+
+let marks = [97, 84, 75, 64,10, 20, 50];
+
+// add
+// marks.splice(2, 0, 101, 103, 104);
+// console.log(marks);
+
+// delete
+//  console.log(marks);
+// marks.splice(3, 2);
+//  console.log(marks);
+
+// console.log(marks);
+// console.log(marks.slice(1, 4));
+
+
+// let totalMarks = marks.concat(marks2);
+// console.log(totalMarks);
+
+// marks.unshift(100);
+// console.log(marks);
+
+// console.log(marks);
+// marks.push(33);
+// console.log(marks);
+// marks.pop();
+// console.log(marks);
+// for(let i=0; i<marks.length; i++){
+//     console.log(marks[i]);
+// }
+
+// for(let i of marks){
+//     console.log(i);
+// }
+
+// let sum =0;
+
+// for(let val of marks){
+//     sum+= val;  // sum = sum + val
+
+
+// console.log(sum);
+
+// let avg = sum / marks.length;
+// console.log(avg);
